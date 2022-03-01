ecc51655682a4e626f5bfddcb30a46c5e3670b95
Sigrid Nicole Ramos <sigridncl@gmail.com>
2022-03-02 03:55:25 +0800

    Added validation pipes using class-validator package

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,20 @@
-export interface AuthDto {
-  email: string,
-  password: string
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+
+//With class validator
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 }
+
+
+//Interface dto Version
+// export interface AuthDto {
+//   email: string,
+//   password: string
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //strips out elements that are not in the dto, mainly for security purposes
+    }),
+  );
+
   await app.listen(6100);
 }
+
 bootstrap();
