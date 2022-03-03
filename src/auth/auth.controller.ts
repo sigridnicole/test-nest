d830d44d491d733bb624b643a5abb6fc338b1808
Sigrid Nicole Ramos <sigridncl@gmail.com>
2022-03-04 01:35:57 +0800

    add basic signup and signin auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,54 +6,44 @@ import {
   Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
 import { AuthDto } from './dto';
-
 @Controller('auth') //global prefix route
 export class AuthController {
   // NestJS dependency injection
   // private - shorthand for definition of this.authservice
-  constructor(private authservice: AuthService) { }
+  constructor(private authservice: AuthService) {}
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({
-      dto
-    })
-    console.log(dto.email)
-    console.log(dto.password)
-    return this.authservice.signup()
+    return this.authservice.signup(dto);
   }
 
   @Post('signin')
-  signin() {
-    // return 'I am signed in'
-    return this.authservice.signin();
+  signin(@Body() dto: AuthDto) {
+    return this.authservice.signin(dto);
   }
-
 
   //Sample express access - not recommended (because you may want to change to another framework, example: fastify)
   //Instead of using the request body, nest js uses DTOs
-  @Post('express')
-  express(@Req() req: Request) {
-    console.log(req.body);
-    return this.authservice.signin();
-  }
+  // @Post('express')
+  // express(@Req() req: Request) {
+  //   console.log(req.body);
+  //   return this.authservice.signin();
+  // }
 
   // SAMPLE VERBOSE PIPING, instead of doing this format, piping can be done inside the dto
-  @Post('signup-verbose-pipe') //POST auth/signup
-  signupVp(
-    @Body('email') email: string,
-    @Body('password', ParseIntPipe) password: string,
-  ) {
-    //Using Nest Pipes
-    console.log({
-      email,
-      typeofEmail: typeof email,
-      typeofPassword: typeof password,
-      password,
-    });
-    return this.authservice.signup();
-  }
-
+  // @Post('signup-verbose-pipe') //POST auth/signup
+  // signupVp(
+  //   @Body('email') email: string,
+  //   @Body('password', ParseIntPipe) password: string,
+  // ) {
+  //   //Using Nest Pipes
+  //   console.log({
+  //     email,
+  //     typeofEmail: typeof email,
+  //     typeofPassword: typeof password,
+  //     password,
+  //   });
+  //   return this.authservice.signup();
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,79 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-// import { User, Bookmark } from '@prisma/client';
-
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { format } from 'path/posix';
 @Injectable()
-export class AuthService{
-  constructor(private prisma: PrismaService) {
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-  }
-  signin() {
-    return 'I am signed in'
+  async signup(dto: AuthDto) {
+    try {
+      //generate password hash
+      const hash = await argon.hash(dto.password);
+      //save the user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select: { // like select in mongodb to return only the specific fields
+        //   id: true,
+        //   email: true,
+        //   createdAt: true
+        // }
+      });
+
+      delete user.hash; // quick and dirty solution for now to prevent the hash from being returned
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          //Prisma's code for @unique constraint
+          throw new ForbiddenException(
+            'Credentials taken.',
+          );
+        }
+      }
+
+      throw Error;
+    }
   }
 
-  signup() {
-    return 'I am signed up'
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect.',
+      );
+
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    console.log({ pwMatches });
+
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect.',
+      );
+
+    delete user.hash;
+    return user;
   }
 }
