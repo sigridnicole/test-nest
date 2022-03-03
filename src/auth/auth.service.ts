1de433f230a5fce6c092d194dd1318e770ed8a4a
Sigrid Nicole Ramos <sigridncl@gmail.com>
2022-03-04 03:35:00 +0800

    added jwt-passport auth with nestjs guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { format } from 'path/posix';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     try {
@@ -73,7 +79,29 @@ export class AuthService {
         'Credentials incorrect.',
       );
 
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret,
+      },
+    );
+
+    return {
+      access_token: token,
+    };
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() Req: Request) {
+    return Req.user;
+  }
+}
