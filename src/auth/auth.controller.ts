466a6373acddeba6d31c4d8e8c84338fa3e90cb5
Sigrid Nicole Ramos <sigridncl@gmail.com>
2022-03-07 11:33:39 +0800

    added custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import {
   Body,
   Controller,
-  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
   Post,
-  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
@@ -18,6 +18,7 @@ export class AuthController {
     return this.authservice.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authservice.signin(dto);
